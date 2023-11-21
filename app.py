8676649fc7bf51bb8c8ad9d5ad1706144e980d72
Ripa <39755690+NasrinRipa@users.noreply.github.com>
2023-11-21 16:54:39 +0600

    changing app.py 3

--- app.py
+++ app.py
@@ -3,21 +3,24 @@ import requests
 
 app = Flask(__name__)
 
+
 def predict_drug(input_text):
     response = requests.post("https://nasrin2023ripa-medicine-library.hf.space/run/predict", json={
-        "data": [
-            input_text
-        ]
+        "data": [input_text]
     }).json()
     
-    # Assuming the response contains 'uses', 'dosage', and 'side_effects' keys
-    drug_details = {
-        "uses": response["uses"],
-        "dosage": response["dosage"],
-        "side_effects": response["side_effects"]
-    }
-    
-    return drug_details
+    # Check if the expected keys exist in the API response
+    if 'uses' in response and 'dosage' in response and 'side_effects' in response:
+        drug_details = {
+            "uses": response["uses"],
+            "dosage": response["dosage"],
+            "side_effects": response["side_effects"]
+        }
+        return drug_details
+    else:
+        # Handle the case where the expected keys are missing in the API response
+        return {"error": "Unexpected response format from the API"}
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
