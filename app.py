01008293e457b48d15c01019b8149954d41f0c33
Ripa <39755690+NasrinRipa@users.noreply.github.com>
2023-11-21 17:10:31 +0600

    changing app.py 5

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-
-# Modify the predict_drug function to return a dictionary with 'uses', 'dosage', 'side_effects' keys
 def predict_drug(input_text):
     response = requests.post("https://nasrin2023ripa-medicine-library.hf.space/run/predict", json={
         "data": [
@@ -12,22 +10,15 @@ def predict_drug(input_text):
         ]
     }).json()
     
-    # Assuming the response contains 'data' key with the required information
     predicted_text = response["data"][0]  # Get the first item from the 'data' list
-    
-    return {
-        "uses": predicted_text.get('uses', ''),
-        "dosage": predicted_text.get('dosage', ''),
-        "side_effects": predicted_text.get('side_effects', '')
-    }
-
+    return predicted_text
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
         input_text = request.form['text']
-        drug_details = predict_drug(input_text)
-        return render_template("results.html", input_text=input_text, drug_details=drug_details)
+        output_text = predict_drug(input_text)
+        return render_template("results.html", input_text=input_text, output_text=output_text)
     else:
         return render_template("index.html")
 
